da5f90b25cc9b486a3ffa25bb5d2552e854ac3dc
ふぁ <yuki@yuki0311.com>
2023-04-09 00:02:46 +0900

    update gen
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- tools/gen.ps1
+++ tools/gen.ps1
@@ -2,3 +2,4 @@ tools/clean.ps1
 java -jar tools/openapi-generator-cli.jar generate -g dart-dio -c tools/openapi-generator-config.yaml
 flutter pub get
 flutter pub run build_runner build --delete-conflicting-outputs
+Copy-Item -Path twitter-openapi/LICENSE.txt -Destination LICENSE.txt -Force
