a8e0dbe8a9baddee3e541d412a8ee6e7cda234a2
ふぁ <yuki@yuki0311.com>
2023-04-20 09:36:38 +0900

    rename
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- twitter_openapi_dart/lib/src/api.dart
+++ twitter_openapi_dart/lib/src/api.dart
@@ -1,48 +1,48 @@
 import 'dart:io';
 import 'dart:math';
 
-import 'package:twitter_openapi_dart/twitter_openapi_dart.dart';
+import 'package:twitter_openapi_dart_generated/twitter_openapi_dart_generated.dart';
 import 'package:dio/dio.dart';
 import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
-import 'package:twitter_openapi_dart_utils/src/auth/header.dart';
+import 'package:twitter_openapi_dart/src/auth/header.dart';
 
-class TwitterOpenapiDartUtils {
-  TwitterOpenapiDart api;
+class TwitterOpenapiDart {
+  TwitterOpenapiDartGenerated api;
 
   static Uri base = Uri.https("twitter.com", "/");
   static Uri all = Uri.https(".twitter.com", "/");
 
   Dio get dio => api.dio;
 
-  TwitterOpenapiDartUtils.fromApiKey({
+  TwitterOpenapiDart.fromApiKey({
     required String ct0,
     required String authToken,
-  }) : api = TwitterOpenapiDart() {
+  }) : api = TwitterOpenapiDartGenerated() {
     api.setApiKey("ct0", ct0);
     api.setApiKey("auth_token", authToken);
   }
 
-  TwitterOpenapiDartUtils.fromCookies({
+  TwitterOpenapiDart.fromCookies({
     required String ct0,
     required String authToken,
-  }) : api = TwitterOpenapiDart() {
+  }) : api = TwitterOpenapiDartGenerated() {
     api.setApiKey("ct0", ct0);
     api.setApiKey("auth_token", authToken);
   }
 
-  TwitterOpenapiDartUtils.fromCookiesPath(
+  TwitterOpenapiDart.fromCookiesPath(
     String cookiePath,
-  ) : api = TwitterOpenapiDart() {
+  ) : api = TwitterOpenapiDartGenerated() {
     dio.interceptors.addAll([
       CookieManager(PersistCookieJar(storage: FileStorage(cookiePath))),
       HeaderAuth(),
     ]);
   }
 
-  TwitterOpenapiDartUtils.fromCookieJar(
+  TwitterOpenapiDart.fromCookieJar(
     CookieJar cookie,
-  ) : api = TwitterOpenapiDart() {
+  ) : api = TwitterOpenapiDartGenerated() {
     dio.interceptors.addAll([CookieManager(cookie), HeaderAuth()]);
   }
   /*
@@ -51,17 +51,17 @@ class TwitterOpenapiDartUtils {
   }
   */
 
-  TwitterOpenapiDartUtils.fromAPI(this.api);
+  TwitterOpenapiDart.fromAPI(this.api);
 }
 
 Future<CookieJar> getGuestCookies() async {
   final cookie = CookieJar();
   final dio = Dio()..interceptors.add(CookieManager(cookie));
-  await dio.requestUri(TwitterOpenapiDartUtils.base, options: Options(method: "GET"));
+  await dio.requestUri(TwitterOpenapiDart.base, options: Options(method: "GET"));
 
   const String charset = '0123456789abcdef';
   final Random random = Random.secure();
   final csrfToken = List.generate(32, (_) => charset[random.nextInt(charset.length)]).join();
-  await cookie.saveFromResponse(TwitterOpenapiDartUtils.all, [Cookie("ct0", csrfToken)]);
+  await cookie.saveFromResponse(TwitterOpenapiDart.all, [Cookie("ct0", csrfToken)]);
   return cookie;
 }
--- twitter_openapi_dart/test/twitter_openapi_dart_utils_test.dart
+++ twitter_openapi_dart/test/twitter_openapi_dart_utils_test.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import 'package:twitter_openapi_dart_utils/twitter_openapi_dart_utils.dart';
+import 'package:twitter_openapi_dart/twitter_openapi_dart.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -11,9 +11,8 @@ void main() {
   });
 
   test('FlutterInappwebviewDio', () async {
-    final client =
-        TwitterOpenapiDartUtils.fromCookieJar(await getGuestCookies());
-    final response = await client.api.getGraphqlApi().getTweetDetail(
+    final client = TwitterOpenapiDart.fromCookieJar(await getGuestCookies());
+    final response = await client.api.getDefaultApi().getTweetDetail(
         variables: jsonEncode({
           "focalTweetId": "1349129669258448897",
           "with_rux_injections": false,
@@ -29,8 +28,7 @@ void main() {
           "responsive_web_graphql_exclude_directive_enabled": true,
           "verified_phone_label_enabled": false,
           "responsive_web_graphql_timeline_navigation_enabled": true,
-          "responsive_web_graphql_skip_user_profile_image_extensions_enabled":
-              false,
+          "responsive_web_graphql_skip_user_profile_image_extensions_enabled": false,
           "tweetypie_unmention_optimization_enabled": true,
           "vibe_api_enabled": true,
           "responsive_web_edit_tweet_api_enabled": true,
@@ -40,8 +38,7 @@ void main() {
           "tweet_awards_web_tipping_enabled": false,
           "freedom_of_speech_not_reach_fetch_enabled": false,
           "standardized_nudges_misinfo": true,
-          "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":
-              false,
+          "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": false,
           "interactive_text_enabled": true,
           "responsive_web_text_conversations_enabled": false,
           "longform_notetweets_rich_text_read_enabled": true,
