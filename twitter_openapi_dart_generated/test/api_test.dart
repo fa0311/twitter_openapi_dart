394d6e43602c2933128902c7d43e18c7367ac7af
ふぁ <yuki@yuki0311.com>
2023-06-14 18:07:18 +0900

    update test
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- twitter_openapi_dart_generated/test/api_test.dart
+++ twitter_openapi_dart_generated/test/api_test.dart
@@ -14,16 +14,13 @@ class HeaderAuth extends Interceptor {
 
   Future<void> readCookies(String path) async {
     final file = new File(path);
-    cookies =
-        (json.decode(await file.readAsString()) as Map).cast<String, String>();
+    cookies = (json.decode(await file.readAsString()) as Map).cast<String, String>();
   }
 
   @override
-  void onRequest(
-      RequestOptions options, RequestInterceptorHandler handler) async {
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
     options.headers.addAll(<String, String>{
-      "x-csrf-token":
-          cookies.entries.firstWhere((e) => e.key == HeaderAuth.ct0).value,
+      "x-csrf-token": cookies.entries.firstWhere((e) => e.key == HeaderAuth.ct0).value,
       "cookie": cookies.entries.fold("", (a, b) => "${a}${b.key}=${b.value};"),
       // "x-client-uuid": "",
     });
@@ -32,21 +29,13 @@ class HeaderAuth extends Interceptor {
 }
 
 bool contentTest(BuiltList<InstructionUnion> instructions) {
-  final item = instructions
-      .expand((e) => e.oneOf.isType(TimelineAddEntries)
-          ? [e.oneOf.value as TimelineAddEntries]
-          : <TimelineAddEntries>[])
-      .first;
+  final item = instructions.expand((e) => e.oneOf.isType(TimelineAddEntries) ? [e.oneOf.value as TimelineAddEntries] : <TimelineAddEntries>[]).first;
 
-  final timelineItem = item.entries.expand((e) =>
-      e.content.oneOf.isType(TimelineTimelineItem)
-          ? [e.content.oneOf.value as TimelineTimelineItem]
-          : <TimelineTimelineItem>[]);
+  final timelineItem =
+      item.entries.expand((e) => e.content.oneOf.isType(TimelineTimelineItem) ? [e.content.oneOf.value as TimelineTimelineItem] : <TimelineTimelineItem>[]);
 
-  final timelineModule = item.entries.expand((e) =>
-      e.content.oneOf.isType(TimelineTimelineModule)
-          ? [e.content.oneOf.value as TimelineTimelineModule]
-          : <TimelineTimelineModule>[]);
+  final timelineModule = item.entries
+      .expand((e) => e.content.oneOf.isType(TimelineTimelineModule) ? [e.content.oneOf.value as TimelineTimelineModule] : <TimelineTimelineModule>[]);
   print(timelineItem.length + timelineModule.length);
   return (timelineItem.length + timelineModule.length) > 0;
 }
@@ -65,47 +54,44 @@ void main() async {
   );
 
   final file = new File("twitter-openapi/src/config/placeholder.json");
-  final config =
-      (json.decode(await file.readAsString()) as Map).cast<String, dynamic>();
+  final config = (json.decode(await file.readAsString()) as Map).cast<String, dynamic>();
   test('getHomeTimeline', () async {
     final response = await client.getTweetApi().getHomeTimeline(
+          pathQueryId: config["HomeTimeline"]!["queryId"].toString(),
           variables: jsonEncode(config["HomeTimeline"]!["variables"]),
           features: jsonEncode(config["HomeTimeline"]!["features"]),
         );
     expect(response.statusCode, 200);
     expect(response.data == null, false);
-    expect(contentTest(response.data!.data.home.homeTimelineUrt.instructions),
-        true);
+    expect(contentTest(response.data!.data.home.homeTimelineUrt.instructions), true);
   });
 
   test('getHomeLatestTimeline', () async {
     final response = await client.getTweetApi().getHomeLatestTimeline(
+          pathQueryId: config["HomeLatestTimeline"]!["queryId"].toString(),
           variables: jsonEncode(config["HomeLatestTimeline"]!["variables"]),
           features: jsonEncode(config["HomeLatestTimeline"]!["features"]),
         );
     expect(response.statusCode, 200);
     expect(response.data == null, false);
-    expect(contentTest(response.data!.data.home.homeTimelineUrt.instructions),
-        true);
+    expect(contentTest(response.data!.data.home.homeTimelineUrt.instructions), true);
   });
 
   test('getListLatestTweetsTimeline', () async {
     final response = await client.getTweetApi().getListLatestTweetsTimeline(
-          variables:
-              jsonEncode(config["ListLatestTweetsTimeline"]!["variables"]),
+          pathQueryId: config["ListLatestTweetsTimeline"]!["queryId"].toString(),
+          variables: jsonEncode(config["ListLatestTweetsTimeline"]!["variables"]),
           features: jsonEncode(config["ListLatestTweetsTimeline"]!["features"]),
         );
     expect(response.statusCode, 200);
     expect(response.data == null, false);
-    expect(
-        contentTest(
-            response.data!.data.list.tweetsTimeline.timeline.instructions),
-        true);
+    expect(contentTest(response.data!.data.list.tweetsTimeline.timeline.instructions), true);
   });
 
   test('getUserByScreenName', () async {
     const name = "elonmusk";
     final response = await client.getUserApi().getUserByScreenName(
+          pathQueryId: config["UserByScreenName"]!["queryId"].toString(),
           variables: jsonEncode(config["UserByScreenName"]!["variables"]),
           features: jsonEncode(config["UserByScreenName"]!["features"]),
         );
@@ -116,139 +102,120 @@ void main() async {
 
   test('ProfileSpotlightsQuery', () async {
     final response = await client.getDefaultApi().getProfileSpotlightsQuery(
-          variables: jsonEncode(config["UserByScreenName"]!["variables"]),
-          features: jsonEncode(config["UserByScreenName"]!["features"]),
+          pathQueryId: config["ProfileSpotlightsQuery"]!["queryId"].toString(),
+          variables: jsonEncode(config["ProfileSpotlightsQuery"]!["variables"]),
+          features: jsonEncode(config["ProfileSpotlightsQuery"]!["features"]),
         );
     expect(response.statusCode, 200);
     expect(response.data == null, false);
-    expect(response.data!.data.userResultByScreenName.result.legacy.screenName,
-        "elonmusk");
+    expect(response.data!.data.userResultByScreenName.result.legacy.screenName, "elonmusk");
   });
 
   test('getUserTweets', () async {
     final response = await client.getTweetApi().getUserTweets(
+          pathQueryId: config["UserTweets"]!["queryId"].toString(),
           variables: jsonEncode(config["UserTweets"]!["variables"]),
           features: jsonEncode(config["UserTweets"]!["features"]),
         );
     expect(response.statusCode, 200);
     expect(response.data == null, false);
-    expect(
-        contentTest(
-            response.data!.data.user.result.timelineV2.timeline.instructions),
-        true);
+    expect(contentTest(response.data!.data.user.result.timelineV2.timeline.instructions), true);
   });
 
   test('getUserTweetsAndReplies', () async {
     final response = await client.getTweetApi().getUserTweetsAndReplies(
+          pathQueryId: config["UserTweetsAndReplies"]!["queryId"].toString(),
           variables: jsonEncode(config["UserTweetsAndReplies"]!["variables"]),
           features: jsonEncode(config["UserTweetsAndReplies"]!["features"]),
         );
     expect(response.statusCode, 200);
     expect(response.data == null, false);
-    expect(
-        contentTest(
-            response.data!.data.user.result.timelineV2.timeline.instructions),
-        true);
+    expect(contentTest(response.data!.data.user.result.timelineV2.timeline.instructions), true);
   });
 
   test('getUserMedia', () async {
     final response = await client.getTweetApi().getUserMedia(
+          pathQueryId: config["UserMedia"]!["queryId"].toString(),
           variables: jsonEncode(config["UserMedia"]!["variables"]),
           features: jsonEncode(config["UserMedia"]!["features"]),
         );
     expect(response.statusCode, 200);
     expect(response.data == null, false);
-    expect(
-        contentTest(
-            response.data!.data.user.result.timelineV2.timeline.instructions),
-        true);
+    expect(contentTest(response.data!.data.user.result.timelineV2.timeline.instructions), true);
   });
 
   test('getLikes', () async {
     final response = await client.getTweetApi().getLikes(
+          pathQueryId: config["Likes"]!["queryId"].toString(),
           variables: jsonEncode(config["Likes"]!["variables"]),
           features: jsonEncode(config["Likes"]!["features"]),
         );
     expect(response.statusCode, 200);
     expect(response.data == null, false);
-    expect(
-        contentTest(
-            response.data!.data.user.result.timelineV2.timeline.instructions),
-        true);
+    expect(contentTest(response.data!.data.user.result.timelineV2.timeline.instructions), true);
   });
 
   test('getBookmarks', () async {
     final response = await client.getTweetApi().getBookmarks(
+          pathQueryId: config["Bookmarks"]!["queryId"].toString(),
           variables: jsonEncode(config["Bookmarks"]!["variables"]),
           features: jsonEncode(config["Bookmarks"]!["features"]),
         );
     expect(response.statusCode, 200);
     expect(response.data == null, false);
-    expect(
-        contentTest(
-            response.data!.data.bookmarkTimelineV2.timeline.instructions),
-        true);
+    expect(contentTest(response.data!.data.bookmarkTimelineV2.timeline.instructions), true);
   });
 
   test('getTweetDetail', () async {
     final response = await client.getTweetApi().getTweetDetail(
+          pathQueryId: config["TweetDetail"]!["queryId"].toString(),
           variables: jsonEncode(config["TweetDetail"]!["variables"]),
           features: jsonEncode(config["TweetDetail"]!["features"]),
         );
     expect(response.statusCode, 200);
     expect(response.data == null, false);
-    expect(
-        contentTest(response
-            .data!.data.threadedConversationWithInjectionsV2.instructions),
-        true);
+    expect(contentTest(response.data!.data.threadedConversationWithInjectionsV2.instructions), true);
   });
   test('getTweetDetail2', () async {
     final response = await client.getTweetApi().getTweetDetail(
-          variables: jsonEncode(config["TweetDetail"]!["variables"]
-            ..addAll({"focalTweetId": "1349265937392930816"})),
+          pathQueryId: config["TweetDetail"]!["queryId"].toString(),
+          variables: jsonEncode(config["TweetDetail"]!["variables"]..addAll({"focalTweetId": "1349265937392930816"})),
           features: jsonEncode(config["TweetDetail"]!["features"]),
         );
     expect(response.statusCode, 200);
     expect(response.data == null, false);
-    expect(
-        contentTest(response
-            .data!.data.threadedConversationWithInjectionsV2.instructions),
-        true);
+    expect(contentTest(response.data!.data.threadedConversationWithInjectionsV2.instructions), true);
   });
 
   test('getFollowers', () async {
     final response = await client.getUserListApi().getFollowing(
+          pathQueryId: config["Followers"]!["queryId"].toString(),
           variables: jsonEncode(config["Followers"]!["variables"]),
           features: jsonEncode(config["Followers"]!["features"]),
         );
     expect(response.statusCode, 200);
     expect(response.data == null, false);
-    expect(
-        contentTest(
-            response.data!.data.user.result.timeline.timeline.instructions),
-        true);
+    expect(contentTest(response.data!.data.user.result.timeline.timeline.instructions), true);
   });
 
   test('getFollowing', () async {
     final response = await client.getUserListApi().getFollowing(
+          pathQueryId: config["Following"]!["queryId"].toString(),
           variables: jsonEncode(config["Following"]!["variables"]),
           features: jsonEncode(config["Following"]!["features"]),
         );
     expect(response.statusCode, 200);
     expect(response.data == null, false);
-    expect(
-        contentTest(
-            response.data!.data.user.result.timeline.timeline.instructions),
-        true);
+    expect(contentTest(response.data!.data.user.result.timeline.timeline.instructions), true);
   });
 
   test('tweet', () async {
     final time = DateTime.now().toIso8601String();
     final response = await client.getPostApi().postCreateTweet(
+          pathQueryId: config["CreateTweet"]!["queryId"].toString(),
           postCreateTweetRequest: PostCreateTweetRequest(
             (e) => e
-              ..variables = PostCreateTweetRequestVariables(
-                  (e) => e..tweetText = "Test[${time}]").toBuilder()
+              ..variables = PostCreateTweetRequestVariables((e) => e..tweetText = "Test[${time}]").toBuilder()
               ..features = PostCreateTweetRequestFeatures((e) => e).toBuilder(),
           ),
         );
@@ -258,11 +225,9 @@ void main() async {
     expect(response.statusCode, 200);
     expect(response.data == null, false);
     final response2 = await client.getPostApi().postDeleteTweet(
+          pathQueryId: config["DeleteTweet"]!["queryId"].toString(),
           postDeleteTweetRequest: PostDeleteTweetRequest(
-            (e) => e
-              ..variables =
-                  PostDeleteTweetRequestVariables((e) => e..tweetId = tweetId)
-                      .toBuilder(),
+            (e) => e..variables = PostCreateRetweetRequestVariables((e) => e..tweetId = tweetId).toBuilder(),
           ),
         );
     print(response2);
@@ -272,8 +237,9 @@ void main() async {
 
   test('createRetweet', () async {
     final response = await client.getPostApi().postCreateRetweet(
+          pathQueryId: config["CreateRetweet"]!["queryId"].toString(),
           postCreateRetweetRequest: PostCreateRetweetRequest(
-            (e) => e..variables = PostDeleteTweetRequestVariables().toBuilder(),
+            (e) => e..variables = PostCreateRetweetRequestVariables().toBuilder(),
           ),
         );
     print(response);
@@ -282,9 +248,9 @@ void main() async {
   });
   test('deleteRetweet', () async {
     final response = await client.getPostApi().postDeleteRetweet(
+          pathQueryId: config["DeleteRetweet"]!["queryId"].toString(),
           postDeleteRetweetRequest: PostDeleteRetweetRequest(
-            (e) =>
-                e..variables = PostDeleteRetweetRequestVariables().toBuilder(),
+            (e) => e..variables = PostDeleteRetweetRequestVariables().toBuilder(),
           ),
         );
     print(response);
@@ -294,8 +260,9 @@ void main() async {
 
   test('favoriteTweet', () async {
     final response = await client.getPostApi().postFavoriteTweet(
+          pathQueryId: config["FavoriteTweet"]!["queryId"].toString(),
           postFavoriteTweetRequest: PostFavoriteTweetRequest(
-            (e) => e..variables = PostDeleteTweetRequestVariables().toBuilder(),
+            (e) => e..variables = PostCreateRetweetRequestVariables().toBuilder(),
           ),
         );
     print(response);
@@ -304,8 +271,9 @@ void main() async {
   });
   test('unfavoriteTweet', () async {
     final response = await client.getPostApi().postUnfavoriteTweet(
+          pathQueryId: config["UnfavoriteTweet"]!["queryId"].toString(),
           postUnfavoriteTweetRequest: PostUnfavoriteTweetRequest(
-            (e) => e..variables = PostDeleteTweetRequestVariables().toBuilder(),
+            (e) => e..variables = PostCreateRetweetRequestVariables().toBuilder(),
           ),
         );
     print(response);
