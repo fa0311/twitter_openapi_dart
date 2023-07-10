354d43a14e6bbd2705d41eebd8bdf72374c4cae7
ふぁ <yuki@yuki0311.com>
2023-07-10 10:10:35 +0900

    add test
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- twitter_openapi_dart/test/api/user_api_test.dart
+++ twitter_openapi_dart/test/api/user_api_test.dart
@@ -12,4 +12,22 @@ main() async {
     print("followedBy: ${legacy.followedBy} following: ${legacy.following}");
     expect(0, 0);
   });
+
+  test('getUserByRestId', () async {
+    final result = await client.getUserApi().getUserByRestId(userId: "44196397");
+    final legacy = result.data.legacy;
+    print(legacy.screenName);
+    print("followedBy: ${legacy.followedBy} following: ${legacy.following}");
+    expect(0, 0);
+  });
+
+  test('getUsersByRestIds', () async {
+    final result = await client.getUserApi().getUsersByRestIds(userIds: ["44196397", "44196397"]);
+    for (final user in result.data) {
+      final legacy = user.legacy;
+      print(legacy.screenName);
+      print("followedBy: ${legacy.followedBy} following: ${legacy.following}");
+    }
+    expect(0, 0);
+  });
 }
